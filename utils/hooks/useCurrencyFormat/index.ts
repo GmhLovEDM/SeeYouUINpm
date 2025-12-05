e482019fd4c27c19489ac31e984b891269e2332d
GmhLovEDM <gmhlovedm@gmail.com>
2025-12-05 13:57:33 +0800

    同步了a3c8efd

--- utils/hooks/useCurrencyFormat/index.ts
+++ utils/hooks/useCurrencyFormat/index.ts
@@ -7,7 +7,7 @@ import {
 
 type NumberInput = MaybeRefOrGetter<number | string | null | undefined>;
 
-interface CurrencyOptions {
+export interface CurrencyOptions {
   decimals?: number; // 保留几位小数，默认 2
   symbol?: string; // 货币符号
   placeholder?: string; // 占位符
